a2bdfd7365bf41ec35ef057bc166576dc275ff55
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-02-08 15:19:15 -0400

    Changes

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { KeyboardContext } from '../contexts/KeyboardContext';
+import '../styles/Login.css'
 
 export const Login = () => {
     const { state, setState } = useContext(KeyboardContext);
 
     const confirmarPIN = () => {
-        {/* TODO implementar el ingreso del PIN por teclado*/}
         if (state == 1234) {
             setState(9876)
         }
@@ -20,7 +20,7 @@ export const Login = () => {
             <h1>
                 {state.pin}
             </h1>
-            <button onClick={confirmarPIN}>
+            <button className='boton' onClick={confirmarPIN()}>
                 Ingresar
             </button>
         </>
---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {  
+  apiKey: "AIzaSyCYbVB6uIMO2zn6q0ktovnGJn8IRo1RNVQ",
+  authDomain: "atm-project-6f00e.firebaseapp.com",
+  projectId: "atm-project-6f00e",
+  storageBucket: "atm-project-6f00e.firebasestorage.app",
+  messagingSenderId: "161146081838",
+  appId: "1:161146081838:web:9974cdfc2849a2e18345bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
