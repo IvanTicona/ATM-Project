cf246e191b00b39b2557fe447c1d0156a36b148d
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-02-12 09:07:16 -0400

    Corrección en la persistencia de datos

--- src/screens/ConsultarSaldo.jsx
+++ src/screens/ConsultarSaldo.jsx
@@ -4,27 +4,33 @@ import '../styles/Button.css'
 import '../styles/Saldo.css'
 
 export const ConsultarSaldo = () => {
-    // TODO se debe cargar el saldo desde la DB
-    const saldo = 0.0
+    const saldo = localStorage.getItem('balance')
 
     let navigate = useNavigate();
-  return (
-    <>
-        <div className='div-vertical'>
-            <h3 className='title'>Su saldo disponible es:</h3>
-            <h3 className='title'>{saldo} Bs</h3>
 
-            <div className='div-horizontal'>
-                <button className='boton' onClick={()=>navigate("/operaciones")}>
-                    <h3 className='texto'>Volver a operaciones</h3>
-                </button>
-                {/* Considerar que al terminar la sesión se debería borrar todos los datos cargados desde el login, como el PIN */}
-                <button className='boton'onClick={()=>navigate("/")}>
-                    <h3 className='texto'>Terminar y cerrar</h3>
-                </button>
+    const closeSession = () => {
+        localStorage.removeItem("account")
+        localStorage.removeItem("balance")
+        localStorage.removeItem("owner")
+    }
+
+    return (
+        <>
+            <div className='div-vertical'>
+                <h3 className='title'>Su saldo disponible es:</h3>
+                <h3 className='title'>{saldo} Bs</h3>
+
+                <div className='div-horizontal'>
+                    <button className='boton' onClick={() => navigate("/operaciones")}>
+                        <h3 className='texto'>Volver a operaciones</h3>
+                    </button>
+                    
+                    <button className='boton' onClick={() => {closeSession(); navigate("/login")}}>
+                        <h3 className='texto'>Terminar y cerrar</h3>
+                    </button>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
