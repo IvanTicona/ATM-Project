c4d8a0d68cb90b5d46f873899ff11c07f03dc62f
pinky <pinkipandarojo@gmail.com>
2025-02-14 10:50:33 -0400

    cambios pinky

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,15 @@
+import '../../styles/Button.css'
+//import imagenSrc from '../../assets/nextIcon.svg'
+
+export const Button = ({direccion, texto, accion}) => {
+    const imagenSrc = ""
+  return (
+    <div onClick={accion} className="boton">
+      {direccion === 'izquierda' && <img src={imagenSrc} alt="icono" className="icono" />}
+      <div className="texto">
+        <h3>{texto}</h3>
+      </div>
+      {direccion === 'derecha' && <img src={imagenSrc} alt="icono" className="icono" />}
+    </div>
+  )
+}
