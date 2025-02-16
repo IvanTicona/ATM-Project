bf594d1de81c3f584914eab0cd8a3d881502cea4
pinky <pinkipandarojo@gmail.com>
2025-02-15 23:48:37 -0400

    porfin

--- src/screens/RetiroRapido.jsx
+++ src/screens/RetiroRapido.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../components/common/Button'
 import { useNavigate } from "react-router"
+import { accountNumber, getAccountData } from '../services/account';
+import { withDraw } from '../services/transactionService';
+import { toast } from 'react-toastify';
 
 
 
 export const RetiroRapido = () => {
     let navigate = useNavigate();
+    const amounts =[50,100,200,500]
+    const [acountBalance,setAccountBalance]=useState(0);
+    const handleSubmit = async (numericAmount) => {
+      try {
+        if (isNaN(numericAmount) || numericAmount <= 0) {
+          toast.error("Por favor, ingresa un monto válido.");
+          return;
+        }
+        toast.info("Enviada solicitud para el retiro de: " + numericAmount.toString() + " Bs.");
+        await withDraw(accountNumber, numericAmount);
+        navigate("/operaciones");
+      } catch (error) {
+        toast.error("Error: " + error.message);
+      }
+    };
+    useEffect(() => {
+      const fetchSaldo = async () => {
+        try {
+          const { balance } = await getAccountData();
+          setAccountBalance(balance);
+        } catch (e) {
+          toast.error("Error al consultar saldo");
+        }
+      };
+  
+      fetchSaldo();
+    }, []);
   return (
     <>
  
         <h1 className='title-atm'>Retiro</h1>
-        <p className="texto">Saldo disponible: <strong>Bs. <span id="balance">1000</span> </strong></p>
+        <p className="texto">Saldo disponible: <strong>Bs. <span id="balance">{acountBalance}</span> </strong></p>
         <div className="buttons">
-            <Button texto = "50 Bs." direccion = "derecha" accion = {()=> navigate("/retiro-rapido")}/>
-            <Button texto = "100 Bs." direccion = "derecha" accion = {()=> navigate("/retiro-rapido")}/>
-            <Button texto = "200 Bs." direccion = "derecha" accion = {()=> navigate("/retiro-rapido")}/>
-            <Button texto = "500 Bs." direccion = "derecha" accion = {()=> navigate("/retiro-rapido")}/>
+            {
+              amounts.map((amount,index)=>{
+                return(
+                  <Button key={index} texto = {`Bs.${amount}`} direccion = "derecha" accion = {()=> handleSubmit(amount)}/>
+                )
+              })
+            }
     </div>
-    <input type="number" id="customAmount" placeholder="Otro monto" />
-    <Button texto = "Retiro" direccion = "derecha" accion = {()=> navigate("/retiro-rapido")}/>
+    <Button texto = "Otro monto" direccion = "derecha" accion = {()=> navigate("/retiro")}/>
  
 
     </>
